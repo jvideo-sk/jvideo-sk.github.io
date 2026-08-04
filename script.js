12121cab026c51176f84a8efa9ea4713b66637ea
JVideo <59378133+jvideo-sk@users.noreply.github.com>
2026-08-04 16:58:39 -0600

    update site

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 console.log("Welcome to jvideo.dev");
+const ageSelector = document.getElementById("age");
 
 function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("show");
 }
+
+function change() {
+    const randomText = texts[Math.floor(Math.random() * texts.length)];
+    parentTextSelector.classList.add("hide");
+
+    setTimeout(() => {
+        textSelector.innerText = randomText;
+        parentTextSelector.classList.remove("hide");
+    }, 500);
+}
+
+setInterval(change, 2000);
+
+change();
+
+const age = ((Date.now() - new Date("2008-9-2")) / (31557600000));
+ageSelector.innerText = age.toFixed(8);
+
+setInterval(() => {
+    const age = ((Date.now() - new Date("2008-9-2")) / (31557600000));
+    ageSelector.innerText = age.toFixed(10);
+}, 1);
